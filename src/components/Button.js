44e59785ee6907903f373d0618eb0cd973030bcf
Miras <miamib531@gmail.com>
2023-05-04 18:39:37 +0600

    feat: implement Animation of pressing a key

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,11 +5,22 @@ export class Button {
         this.newButton.textContent = keyData;
         this.newButton.setAttribute('data-content', keyData);
         this.newButton.classList.add('btn');
-        this.newButton.addEventListener('mousedown', () => cb(keyData));
+        this.newButton.addEventListener('mousedown', () => {
+            cb(keyData);
+            this.newButton.classList.add('active');
+        });
+        this.newButton.addEventListener('mouseup', () => {
+            this.newButton.classList.remove('active');
+        });
         parent.append(this.newButton);
     }
 
     onKeyDown() {
         this.handleKeyDown(this.newButton.textContent);
+        this.newButton.classList.add('active');
+    }
+
+    onKeyUp() {
+        this.newButton.classList.remove('active');
     }
 }
